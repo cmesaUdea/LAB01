c5753fb911decdce0e25954c258cf35feef649b1
Carlos Mesa <cmesaroldan@gmail.com>
2023-08-23 18:28:32 -0500

    Se crea el archivo para el laboratorio 1

---
+++ lab1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 1;
+    cout << "Hello World!" << endl;
+
+    switch (n){
+    case 1:
+        cout << "Hola mundo" << endl;
+        break;
+
+    case 2:
+        cout << "Hola mundo 2" << endl;
+        break;
+    }
+
+    return 0;
+}
