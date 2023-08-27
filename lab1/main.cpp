155f88c3a4c6d7e544ded716de7f92aaa8b8a988
Carlos Mesa <cmesaroldan@gmail.com>
2023-08-27 08:17:37 -0500

    Se avanza en el laboratorio, pendiente implementar el while

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -15,58 +15,116 @@ bool esVocal(char caracter2){
 
 }
 
+// Funciones punto 4
+
 
 
 
 int main()
 {
     int n = 2;
+    while(n != 0){
 
+        cout << "Ingresa el numero del problema que desea observar:"<< endl;
+        cin >> n;
+        switch (n){
+        case 1:
+            char caracter;
+            bool vocal;
+            cout << "Ha elegido el problema 1:" << endl;
+            cout << "Problema 1. Escriba un programa que identique si un carácter ingresado es una vocal, una consonante o ninguna de las 2 e imprima un mensaje segun el caso" <<endl;
+            cout << "Ingrese la vocal o consonante" << endl;
+            cin >> caracter;
 
-    switch (n){
-    case 1:
-        char caracter;
-        bool vocal;
-        cout << "Ha elegido el problema 1:" << endl;
-        cout << "Problema 1. Escriba un programa que identique si un carácter ingresado es una vocal, una consonante o ninguna de las 2 e imprima un mensaje segun el caso" <<endl;
-        cout << "Ingrese la vocal o consonante" << endl;
-        cin >> caracter;
+            if (caracter >= 'A' && caracter <= 'Z' || caracter >= 'a' && caracter <= 'z'){
+                vocal = esVocal(caracter);
 
-        if (caracter >= 'A' && caracter <= 'Z' || caracter >= 'a' && caracter <= 'z'){
-            vocal = esVocal(caracter);
+                if(vocal){
+                    cout << caracter << " es una vocal." << endl;
+                }else{
+                    cout << caracter << " es una consonante" << endl;
+                }
 
-            if(vocal){
-                cout << caracter << " es una vocal." << endl;
             }else{
-                cout << caracter << " es una consonante" << endl;
+                cout << "no es una letra" << endl;
             }
 
-        }else{
-            cout << "no es una letra" << endl;
+            break;
+
+        case 2:{
+
+            int money = 0;
+            cout << "Ha elegido el problema 2" << endl;
+            cout << "Ingrese una cantidad de dinero:"<<endl;
+            cin >> money;
+
+            int cambio[10] = {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50};
+
+            for(int i = 0; i <= 10; i++){
+                cout << cambio[i] << " : "<<money/cambio[i] << endl;
+                money = money - ( (money/cambio[i]) * cambio[i]);
+
+                if (money == 0 || money < 50){
+                    cout << "Faltante: " << money<< endl;
+                    break;
+                }
+            }
+
+            break;
         }
 
-        break;
+
+        case 3:{
+
+            int mes = 0, dia = 0;
+
+            cout << "Usted ha elegido el problema 3:"<<endl;
+            cout << "Ingrese el mes" << endl;
+            cin >> mes;
+
+            cout << "Ingrese el dia: " << endl;
+            cin >> dia;
+
+            if(mes == 12 || mes == 10 || mes == 8 || mes == 7 || mes == 5 || mes == 3 || mes == 1 ){
+
+                if(dia >= 1 && dia <= 31){
+                    cout << mes <<"/"<< dia << " es una fecha valida." << endl;
+                }else{
+                    cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
+                }
+
+            }else if(mes == 4 || mes == 6 || mes ==9 || mes == 11 ){
+
+                if(dia >= 1 && dia <= 30){
+                    cout << mes <<"/"<< dia << " es una fecha valida." << endl;
+                }else{
+                    cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
+                }
+
+            }else if( mes == 2){
+                    if(dia == 28){
+                        cout << mes <<"/"<< dia << " es valido en bisisesto." << endl;
+                    }else if(dia >= 1 && dia <= 28){
+                        cout << mes <<"/"<< dia << " es una fecha valida." << endl;
+                    }else{
+                        cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
+                    }
 
 
-    case 2:
+            }else{
+                cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
+            }
 
-        int money = 0, total = 0;
-        cout << "Ha elegido el problema 2" << endl;
-        cout << "Ingrese una cantidad de dinero:"<<endl;
-        cin >> money;
-
-        int cambio[10] = {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50};
-
-        for(int i = 0; i <= 10; i++){
-            cout << cambio[i] << " : "<<money/cambio[i] << endl;
-            money = money - ( (money/cambio[i]) * cambio[i]);
+            break;
         }
 
-        break;
+        case 4:{
 
 
 
 
+        }
+    }
 
     return 0;
 
