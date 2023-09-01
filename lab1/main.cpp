5be06451a4b80be26c8eef8b47c33970df5ad1c0
Carlos Mesa <cmesaroldan@gmail.com>
2023-08-31 20:11:39 -0500

    Pendiente punto 5, adelanto del punto 6 y 7

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -26,13 +26,65 @@ bool isHour(int hour){
     }
 }
 
+void sumaHora(int hora, int duty){
 
+    int horaHour = 0, minutosHour = 0, horaDuty = 0, minutosDuty = 0, horaFinal = 0, minutoFinal = 0;
+
+    horaHour = hora / 100;
+    minutosHour = hora % 100;
+
+    horaDuty = duty / 100;
+    minutosDuty = duty % 100;
+
+    horaFinal = horaHour + horaDuty;
+    if(horaFinal > 23){
+        horaFinal = horaFinal % 24;
+    }
+
+    minutoFinal = minutosHour + minutosDuty;
+    if(minutoFinal > 59){
+        horaFinal = horaFinal + (minutoFinal / 60);
+        minutoFinal = minutoFinal % 60;
+
+    }
+
+    cout << "la hora es : " << horaFinal << minutoFinal << endl;
+
+}
+
+//Función para punto 6
+int factorial(int n){
+    int facto = 1;
+    if (n == 0){
+        return 1;
+    }else{
+        for(int i = n ; i != 0; i--){
+            facto *= i;
+        }
+        return facto;
+    }
+}
+
+//Función para punto 7
+
+int fibonacci(int lim){
+
+    int a = 1, b = 1, c = 0;
+
+    do{
+        c = a + b;
+        a = b;
+        b = c;
+
+    } while(a <= lim);
+
+    return a;
+
+}
 
 int main()
 {
-    int n = 2;
-
-
+    int n;
         cout << "Ingresa el numero del problema que desea observar:"<< endl;
         cin >> n;
         switch (n){
@@ -82,7 +134,7 @@ int main()
         }
 
 
-        case 3:{
+        case 3: {
 
             int mes = 0, dia = 0;
 
@@ -93,7 +145,8 @@ int main()
             cout << "Ingrese el dia: " << endl;
             cin >> dia;
 
-            if(mes == 12 || mes == 10 || mes == 8 || mes == 7 || mes == 5 || mes == 3 || mes == 1 ){
+            if(mes == 12 || mes == 10 || mes == 8 || mes == 7 || mes == 5 || mes == 3 || mes == 1 )
+            {
 
                 if(dia >= 1 && dia <= 31){
                     cout << mes <<"/"<< dia << " es una fecha valida." << endl;
@@ -101,15 +154,21 @@ int main()
                     cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
                 }
 
-            }else if(mes == 4 || mes == 6 || mes ==9 || mes == 11 ){
+            }else if(mes == 4 || mes == 6 || mes ==9 || mes == 11 )
+            {
 
-                if(dia >= 1 && dia <= 30){
+                if(dia >= 1 && dia <= 30)
+                {
                     cout << mes <<"/"<< dia << " es una fecha valida." << endl;
-                }else{
+
+                }else
+                {
                     cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
                 }
 
-            }else if( mes == 2){
+            }else if( mes == 2)
+            {
+
                     if(dia == 28){
                         cout << mes <<"/"<< dia << " es valido en bisisesto." << endl;
                     }else if(dia >= 1 && dia <= 28){
@@ -119,21 +178,23 @@ int main()
                     }
 
 
-            }else{
+
+            }else
+            {
                 cout << mes <<"/"<< dia << " es una fecha invalida." << endl;
             }
 
             break;
         }
 
-        case 4:{
+        case 4: {
 
             int hour = 0, duty = 0;
 
             cout << "Usted ha elegido el problema 3:"<<endl;
             cout << "Ingrese la hora en formato HH:MM" << endl;
             cin >> hour;
-            cout << isHour(hour);
+
 
             if(isHour(hour) == 0){
                 cout << hour << " es un tiempo invalido" << endl;
@@ -147,16 +208,48 @@ int main()
                 break;
             }
 
-
-
-
-
+            sumaHora( hour, duty );
 
 
 
         }
-    }
+        case 5:{
+            cout << " En proceso "<< endl;
+            break;
+        }
+        case 6:{
+            int aprox = 0;
+            float total = 0;
 
+            cout << "Usted ha elegido el problema 6: "<< endl;
+            cout << " Ingrese el número de elementos usados en la apróximación: " << endl;
+            cin >> aprox;
+
+
+            for (int i = 0; i < aprox ; i++){
+
+                total = total + (1.0 / factorial(i));
+
+            }
+            cout << total << endl;
+            break;
+        }
+
+        case 7:{
+            int lim = 0;
+            cout << "Usted ha elegido el problema 7: "<< endl;
+            cout << "Ingrese el número hasta el cual desea que se genere la serie de fibonacci: " << endl;
+            cin >> lim;
+
+            fibonacci(lim);
+
+            cout << fibonacci(lim) << endl;
+
+        }
+
+
+
+    }
     return 0;
 
 }
